c2fe260e134b91bd07edb7a2d7ceb24253cd1d89
Matthias Kunnen <matthias.kunnen@gmail.com>
2020-04-25 20:51:24 +0200

    Remove all unused imports

--- Data/Scripts/MeridiusIX/ProjectionsToAssembler.cs
+++ Data/Scripts/MeridiusIX/ProjectionsToAssembler.cs
@@ -1,25 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sandbox.Common;
-using Sandbox.Common.ObjectBuilders;
 using Sandbox.Definitions;
-using Sandbox.Game;
-using Sandbox.Game.Entities;
 using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI;
-using Sandbox.ModAPI.Interfaces;
 using Sandbox.ModAPI.Interfaces.Terminal;
-using SpaceEngineers.Game.ModAPI;
 using VRage;
 using VRage.Game;
 using VRage.Game.Components;
-using VRage.Game.Entity;
 using VRage.Game.ModAPI;
 using VRage.ModAPI;
-using VRage.ObjectBuilders;
 using VRage.Utils;
 using VRageMath;
 
